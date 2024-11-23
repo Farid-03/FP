5cb4aeefe009289eb136ec9099a31cc88c932e8c
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2024-11-23 16:13:37 +0400

    Adjust layout and design of email subscription form

--- src/components/join/Join.jsx
+++ src/components/join/Join.jsx
@@ -11,12 +11,14 @@ const Join = () => {
 					<p>Type your email down below and be young wild generation</p>
 				</div>
 				<div className='join__form'>
-					<input
-						type='email'
-						className='join__input'
-						placeholder='Add your email here'
-					/>
-					<button className='join__btn'>Send</button>
+					<div className='join__input-wrapper'>
+						<input
+							type='email'
+							className='join__input'
+							placeholder='Add your email here'
+						/>
+						<button className='join__btn'>Send</button>
+					</div>
 				</div>
 			</div>
 		</section>
